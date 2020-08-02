1461620dfad78c25eebafbb3fedc65cac2b40ae8
kahheng <kahheng98q@gmail.com>
2020-08-02 10:26:58 +0800

    navi

--- app/src/main/java/com/example/chcook/premium.java
+++ app/src/main/java/com/example/chcook/premium.java
@@ -1,23 +1,41 @@
 package com.example.chcook;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
+import android.speech.RecognitionListener;
 import android.speech.RecognitionService;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class premium extends Fragment {
-
+private  View view;
+private ConstraintLayout constraintLayout;
+private SpeechRecognizer speechRecognizer;
+private Intent speechRecognizerIntent;
+private String keeper="";
 
     public premium() {
         // Required empty public constructor
@@ -28,13 +46,98 @@ public class premium extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_premium, container, false);
+        view= inflater.inflate(R.layout.fragment_premium, container, false);
+        checkvoice();
+        speechRecognizer=SpeechRecognizer.createSpeechRecognizer(getActivity());
+        speechRecognizerIntent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> matchesF =results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if(matchesF!=null){
+                    keeper=matchesF.get(0);
+                    Toast.makeText(getActivity(), "Keep:"+keeper, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+
+
+        constraintLayout=view.findViewById(R.id.CLayout);
+        constraintLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        speechRecognizer.startListening(speechRecognizerIntent);
+                        keeper="";
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        speechRecognizer.stopListening();
+                        break;
+                }
+                return false;
+            }
+        });
+
+        return view;
     }
 
-    private void speak(){
-        Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-    }
+//    private void speak(){
+//        Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+//                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//    }
 
+    private void checkvoice(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            if(!(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.RECORD_AUDIO)== PackageManager.PERMISSION_GRANTED)){
+                Intent intent=new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package"+getActivity().getPackageName()));
+                startActivity(intent);
+                getActivity().finish();
+            }
+        }
+    }
 }
