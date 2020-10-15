46c66578f18aa2bce2f0a98bbdf1914dc7fec01f
jackychia0329 <chiayj-am17@student.tarc.edu.my>
2020-10-15 13:11:44 +0800

    jacky2

---
+++ app/src/main/java/com/example/chcook/YangJie/StaffMainPage.java
@@ -0,0 +1,15 @@
+package com.example.chcook.YangJie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.chcook.R;
+
+public class StaffMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_main_page);
+    }
+}
