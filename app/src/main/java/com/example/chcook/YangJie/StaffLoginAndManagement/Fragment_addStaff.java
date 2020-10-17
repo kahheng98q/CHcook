43392d88e5d03aa3731d758e0a1a0364bb045f58
jackychia0329 <chiayj-am17@student.tarc.edu.my>
2020-10-17 18:04:38 +0800

    jacky5

---
+++ app/src/main/java/com/example/chcook/YangJie/StaffLoginAndManagement/Fragment_addStaff.java
@@ -0,0 +1,21 @@
+package com.example.chcook.YangJie.StaffLoginAndManagement;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.chcook.R;
+
+public class Fragment_addStaff extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_addstaff, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/chcook/YangJie/StaffMainPage.java
+++ app/src/main/java/com/example/chcook/YangJie/StaffMainPage.java
@@ -2,25 +2,35 @@ package com.example.chcook.YangJie;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.chcook.R;
+import com.example.chcook.YangJie.StaffLoginAndManagement.Fragment_addStaff;
+import com.example.chcook.YangJie.StaffLoginAndManagement.StaffLogin;
 import com.example.chcook.YangJie.StaffLoginAndManagement.StaffPersonalAccount;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class StaffMainPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class StaffMainPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,21 +38,31 @@ public class StaffMainPage extends AppCompatActivity implements NavigationView.O
 
         drawerLayout = findViewById(R.id.drawer_staff);
         navigationView = findViewById(R.id.nav_view_staff);
+        toolbar = findViewById(R.id.toolbarStaff);
+        setSupportActionBar(toolbar);
 
         navigationView.bringToFront();
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+
+        //load default fragment
+        fragmentManager =getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_staff,new MainFragment_staff());
+        fragmentTransaction.commit();
+
+
         navigationView.setNavigationItemSelectedListener(this);
-        navigationView.setCheckedItem(R.id.nav_home);
+        navigationView.setCheckedItem(R.id.nav_add_staff);
     }
 
-    public void onBackPressed(){
+    public void onBackPressed() {
 
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        }else{
+        } else {
             super.onBackPressed();
         }
 
@@ -50,15 +70,32 @@ public class StaffMainPage extends AppCompatActivity implements NavigationView.O
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+//        navigationView.setCheckedItem(item.getItemId());
+        switch (item.getItemId()) {
+
             case R.id.nav_home:
+                //navigationView.setCheckedItem(R.id.nav_home);
+                fragmentManager =getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_staff,new MainFragment_staff());
+                fragmentTransaction.commit();
+                break;
+            case R.id.nav_add_staff:
+//                navigationView.setCheckedItem(R.id.nav_add_staff);
+                fragmentManager =getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_staff,new Fragment_addStaff());
+                fragmentTransaction.commit();
                 break;
             case R.id.nav_personal_acc:
                 Intent intent1 = new Intent(this, StaffPersonalAccount.class);
                 startActivity(intent1);
                 break;
+
             case R.id.nav_logout:
-                Toast.makeText(this,"logout",Toast.LENGTH_SHORT).show();
+                FirebaseAuth.getInstance().signOut();
+                Intent intent =new Intent(getApplicationContext(),StaffLogin.class);
+                startActivity(intent);
                 break;
 
         }
