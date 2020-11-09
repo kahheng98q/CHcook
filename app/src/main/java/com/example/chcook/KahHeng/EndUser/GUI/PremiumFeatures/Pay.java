d5ae1044d31dede0c7c84810532a16449fc80cc9
kahheng <kahheng98q@gmail.com>
2020-11-09 10:17:47 +0800

    price

--- app/src/main/java/com/example/chcook/KahHeng/EndUser/GUI/PremiumFeatures/Pay.java
+++ app/src/main/java/com/example/chcook/KahHeng/EndUser/GUI/PremiumFeatures/Pay.java
@@ -3,10 +3,13 @@ package com.example.chcook.KahHeng.EndUser.GUI.PremiumFeatures;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +18,12 @@ import com.example.chcook.KahHeng.EndUser.DA.UserDA;
 import com.example.chcook.KahHeng.EndUser.GUI.MainPage;
 import com.example.chcook.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.paypal.android.sdk.payments.PayPalConfiguration;
 import com.paypal.android.sdk.payments.PayPalPayment;
 import com.paypal.android.sdk.payments.PayPalService;
@@ -24,11 +33,13 @@ import com.paypal.android.sdk.payments.PaymentConfirmation;
 import java.math.BigDecimal;
 
 public class Pay extends AppCompatActivity {
-    private CardForm cardForm;
     private Button btnPay;
+    private TextView txtPrice;
     private AlertDialog.Builder alertBuilder;
     private FirebaseAuth firebaseAuth;
-    private String price="15";
+    private FirebaseDatabase database;
+
+    private String price="";
     public static final int PAYPAL_REQUEST_CODE=7171;
     private static  final String PAYPAL_CLIANT_ID="AavoT6futTDug1UjDqQb2SNhxcv4es037vbEHN03rIq4i2J-pMqjd3h2leejKaimovIr85qipFkIVttc";
     private static PayPalConfiguration config=new PayPalConfiguration()
@@ -39,14 +50,16 @@ public class Pay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
-
-        firebaseAuth = FirebaseAuth.getInstance();
+        connectFirebase();
+        getPrice();
+//        firebaseAuth = FirebaseAuth.getInstance();
 
         Intent intent=new Intent(this,PayPalService.class);
         intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
         startService(intent);
+        txtPrice=findViewById(R.id.txtPrice);
+//        textView.setText("");
 
-//        cardForm = findViewById(R.id.card_form);
         btnPay = findViewById(R.id.btnPay);
 
         btnPay.setOnClickListener(new View.OnClickListener() {
@@ -87,5 +100,43 @@ public class Pay extends AppCompatActivity {
         intent.putExtra(PaymentActivity.EXTRA_PAYMENT,payPalPayment);
         startActivityForResult(intent,PAYPAL_REQUEST_CODE);
 
+    }
+
+    private void getPrice(){
+//        DatabaseReference ref = database.getReference("Users").limitToLast(3);
+        DatabaseReference priceRef = database.getReference("ManagePrice");
+        priceRef.orderByChild("EditTime").limitToLast(1).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if (dataSnapshot.exists()) {
+                    price = dataSnapshot.child("Price").getValue(String.class);
+                    txtPrice.setText("Your only need to pay RM "+price);
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public void connectFirebase() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
     }
 }
